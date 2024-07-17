06eca45a3b05772c61e17abd5d94dbf8f8b696e5
Naonidh Singh <naonidhsingh123@gmail.com>
2024-07-17 18:18:34 +0530

    .

--- routes/Test.js
+++
@@ -1,9 +0,0 @@
-const express  = require("express");
-
-const router  = express.Router();
-
-const {testController} = require("../controllers/Test");
-
-router.post("/dummy",testController);
-
-module.exports = router;
--- utils/deleteImage.js
+++
@@ -1,5 +0,0 @@
-const cloudinary = require("cloudinary").v2;
-
-exports.deleteImageFromCloudinary = async (file) => {
-  return await cloudinary.uploader.destroy(file);
-};
